01f00742c7814a72bc16d16fbe3c523a1e154e07
Quan Nguyen <jacob.nguyenaq@gmail.com>
2025-06-09 11:20:06 -0500

    Fixdd author.

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ function App() {
   return (
     <>
       <div className='relative w-screen h-screen overflow-hidden bg-gray-100 dark:bg-neutral-900 transition-colors'>
-        <div className='absolute top-1/4 left-1/10 size-50 rounded-full bg-radial from-yellow-600 to-gray-100 to-70% dark:to-neutral-900 transition-colors'></div>
-        <div className='absolute top-2/4 left-7/10 size-50 rounded-full bg-radial from-yellow-600 to-gray-100 to-70% dark:to-neutral-900 transition-colors'></div>
+        <div className='absolute top-1/5 left-1/10 size-150 rounded-full bg-radial from-yellow-600 to-gray-100 to-70% dark:to-neutral-900 transition-colors'></div>
+        <div className='absolute top-2/4 left-7/10 size-100 rounded-full bg-radial from-yellow-600 to-gray-100 to-70% dark:to-neutral-900 transition-colors'></div>
         <div  className='absolute top-7 left-7' >
           <ToggleDarkMode/>
         </div>
+        <div className='absolute bottom-0 left-1/2 -translate-x-1/2 text-center dark:text-gray-100 transition-colors'>&copy; 2025 Quan Nguyen</div>
       </div>
     </>
   )
